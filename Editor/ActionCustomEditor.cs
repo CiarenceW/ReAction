99884beac784fa55e7f8f9e85b1d8a6c3958758f
CiarenceW <112902813+CiarenceW@users.noreply.github.com>
2025-07-03 13:23:02 +0200

    Made a custom editor to yell at people who make a [Property] ReAction.Action

---
+++ Editor/ActionCustomEditor.cs
@@ -0,0 +1,19 @@
+﻿#if SANDBOX
+
+namespace ReActionPlugin.Editor
+{
+	[CustomEditor(typeof(ReAction.Action))]
+	public class ActionCustomEditor : ControlObjectWidget
+	{
+		public ActionCustomEditor(SerializedProperty property) : base(property, false)
+		{
+			Layout = Layout.Row();
+			Layout.Spacing = 4;
+
+			var piss = new Label("Yeah don't do that LOL");
+
+			Layout.Add(piss);
+		}
+	}
+}
+#endif
